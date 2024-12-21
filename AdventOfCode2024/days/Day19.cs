8a4c6e38588f39362339207dabc165c04dd01e87
Rebix <timon@set-richter.de>
2024-12-21 14:50:59 +0100

    Completed Day 19 :)

--- AdventOfCode2024/days/Day19.cs
+++ AdventOfCode2024/days/Day19.cs
@@ -2,7 +2,55 @@ namespace AdventOfCode2024.days;
 
 public class Day19() : Day(19)
 {
+    private readonly Dictionary<string, long> countCache = new();
+    private readonly Dictionary<string, bool> evaluateCache = new();
+
     protected override void Run(bool isPart2 = false)
     {
+        var patterns = Input[0].Split(", ").ToList();
+        var designs = Input.Skip(2).ToList();
+
+        if (isPart2)
+        {
+            var total = designs.Sum(design => CountDesign(design, patterns));
+            Console.WriteLine(total);
+        }
+        else
+        {
+            var count = designs.Count(design => EvaluateDesign(design, patterns));
+            Console.WriteLine(count);
+        }
+    }
+
+    private bool EvaluateDesign(string design, List<string> patterns)
+    {
+        if (evaluateCache.ContainsKey(design)) return evaluateCache[design];
+
+        if (design.Length == 0) return true;
+
+        foreach (var pattern in patterns)
+            if (design.StartsWith(pattern) && EvaluateDesign(design.Substring(pattern.Length), patterns))
+            {
+                evaluateCache[design] = true;
+                return true;
+            }
+
+        evaluateCache[design] = false;
+        return false;
+    }
+
+    private long CountDesign(string design, List<string> patterns)
+    {
+        if (countCache.ContainsKey(design)) return countCache[design];
+
+        if (design.Length == 0) return 1;
+
+        long count = 0;
+        foreach (var pattern in patterns)
+            if (design.StartsWith(pattern))
+                count += CountDesign(design.Substring(pattern.Length), patterns);
+
+        countCache[design] = count;
+        return count;
     }
 }
--- AdventOfCode2024/obj/Debug/net9.0/AdventOfCode2024.AssemblyInfo.cs
+++ AdventOfCode2024/obj/Debug/net9.0/AdventOfCode2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068ad0972518c4677fca38632b6c367e66790f6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24e05c20fe9d01496e7ada8ea227ea719ebcc8ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
